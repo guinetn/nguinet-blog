27decbcf6414917dfb0ecd3a6a6d213829748c23
Nicolas Guinet <nguinet.pro@gmail.com>
2022-07-26 22:02:45 +0200

    Fix vercel path on ContextConfig

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-import { ConfigContext } from '../components/ConfigContext';
+import { ConfigContext } from './ConfigContext';
 
 export default function Layout({ children, home }) { 
 
