5868335a442d6f0f562b6bd792936830f067d461
Nicolas Guinet <nguinet.pro@gmail.com>
2022-07-28 12:32:21 +0200

    Customize homepage buttons

--- pages/me/me.js
+++ pages/me/me.js
@@ -6,30 +6,33 @@ export default function Me() {
 	return (
 		 <ConfigContext.Consumer>
     	 { config =>
-			 <section style={{marginTop:'10em'}}>
+			<section style={{marginTop:'10em'}}>
 
-			  <div className={style.aboutme}>
+				<div className={style.aboutme}>
 
-			  	<div className={`${style.hello} roll-in`}>	
-	          		<h1>Hello I'm Nicolas</h1>
-	          		<div>I <span>build</span> robust custom desktop/web solutions for the <span>satisfaction</span> of my clients</div>
-	          		<div>
-	          		
-			  			<button>My projects</button>
-			  			<button>Get in touch</button>
-			  		</div>
-			  	</div>
+					<div className={`${style.hello} roll-in`}>	
+						<h1>Nicolas Guinet</h1>
 
-			  	<div className={style.profileSize}>		
-		          <img
-		          	className={`${style.profile} fade-in-down`} 
-		            src="/images/profile.jpg"
-		            height={256}
-		            width={256}
-		            alt={config.name} />
-			  </div>
+						<div className='relative'>
+							<div className={style.role}>Product developer</div>
+							I <span>build</span> robust custom desktop/web solutions for the <span>satisfaction</span> of my clients
+						</div>
+						<div className='halign'>
+							<div>
+								<button className='blue-button'>My projects</button>
+								<button className='blue-button'>Get in touch</button>
+							</div>
+						</div>
 
-			  </div>
+					</div>
+
+					<div className={style.profileSize}>		
+						<img className={`${style.profile} fade-in-down`} 
+						src="/images/profile.jpg"
+						height={256} width={256} alt={config.name} />
+					</div>
+
+				</div>
 	         
 	        </section>
           }
